1b773b14d04c561156540f23606e6f1d5d1ed0e2
中山佳重 <nakayama_yoshie@manabito.biz>
2020-05-10 23:52:33 +0900

    - Add some components.  ( PickerCardGroup, PrimaryButton, SecondaryButton, ButtonGroup, SettingWindow ) - Change a package-lock.json.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
+import { StyleSheet, View, Picker, SafeAreaView } from 'react-native';
+import { Text, Button } from 'react-native-elements';
 
 class NumPicker extends React.Component {
     constructor(props) {
@@ -23,21 +24,108 @@ class NumPicker extends React.Component {
     }
 }
 
+function PickerCardGroup() {
+    return (
+        <View>
+            <section>
+                <h2><Text h4>目標</Text></h2>
+                <NumPicker selected={0} max={50}/>
+                <Text>回</Text>
+                <NumPicker selected={1} max={10}/>
+                <Text>セット</Text>
+            </section>
+            <section>
+                <h2><Text h4>インターバル</Text></h2>
+                <NumPicker selected={0} max={9}/>
+                <Text>分</Text>
+                <NumPicker selected={0} max={59}/>
+                <Text>秒</Text>
+            </section>
+            <section>
+                <h2><Text h4>間隔(ピッチ)</Text></h2>
+                <Text>1回に</Text>
+                <NumPicker selected={1} max={10}/>
+                <Text>秒かける</Text>
+            </section>
+        </View>
+    );
+}
+
+function PrimaryButton(props) {
+    return (
+        <Button
+            title={props.value}
+            type="solid"
+            buttonStyle={styles.button}
+        />
+    );
+}
+
+function SecondaryButton(props) {
+    if (props.isDesabled) {
+        return (
+            <Button
+                title={props.value}
+                type="solid"
+                disabled
+                buttonStyle={styles.button}
+            />
+        );
+    } else {
+        return (
+            <Button
+                title={props.value}
+                type="solid"
+                buttonStyle={styles.button}
+            />
+        );
+    }
+}
+
+function ButtonGroup() {
+    return (
+        <nav>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton value="スタート"/>
+                <SecondaryButton value="キャンセル" isDesabled={true} />
+            </View>
+        </nav>
+    );
+}
+
+function SettingWindow() {
+    return (
+        <section>
+            <h1>設定画面</h1>
+            <PickerCardGroup/>
+            <ButtonGroup/>
+        </section>
+    );
+}
+
 export default function App() {
   return (
-    <View>
-      <NumPicker selected={0} max={50} />
-      <NumPicker selected={1} max={10} />
-      <NumPicker selected={0} max={9} />
-      <NumPicker selected={0} max={59} />
-      <NumPicker selected={1} max={10} />
-    </View>
+    <SafeAreaView>
+        <SettingWindow/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  pickerItem: {
-    height: 200,
-    width: 100
-  },
+    pickerItem: {
+        height: 200,
+        width: 100
+    },
+    button: {
+        backgroundColor: '#f87c54',
+        borderRadius: 150,
+        paddingTop: 20,
+        paddingBottom: 20,
+        width: 150
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
 });
