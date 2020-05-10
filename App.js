42b47aa390e116c8719591e20cf9e2766369509a
中山佳重 <nakayama_yoshie@manabito.biz>
2020-05-10 22:04:45 +0900

    - Add the NumPicker component. - Add a package-lock.json #1

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
+
+class NumPicker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let array = [];
+        const maxNum = this.props.max;
+        for (let i = 1; i <= maxNum; i++) {
+            array.push(i);
+        }
+        return(
+            <Picker
+                selectedValue={this.props.selected}
+                style={styles.pickerItem}>
+                {array.map((item) => {
+                    return (<Picker.Item key={item} label={item.toString()} value={item} />)
+                })}
+            </Picker>
+        );
+    }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <NumPicker selected={0} max={50} />
+      <NumPicker selected={1} max={10} />
+      <NumPicker selected={0} max={9} />
+      <NumPicker selected={0} max={59} />
+      <NumPicker selected={1} max={10} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  pickerItem: {
+    height: 200,
+    width: 100
   },
 });
