8558294531c3d5f1fd4fb59d12cca86d0849c8a0
中山佳重 <nakayama_yoshie@manabito.biz>
2020-05-11 23:10:40 +0900

    - Add some components.  ( NowSetTime, TotalSetTime, SetDisplayBar, BackgroundCircle, CountCircle, NowTime, TotalTime, TimeDisplayGroup, NowSecond, PitchText, CountNumGroup, IntervalMinutes, IntervalSeconds, IntervalNumGroup, CircleDisplayGroup ) - Change a package-lock.json. #1

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Picker, SafeAreaView } from 'react-native';
 import { Text, Button } from 'react-native-elements';
+import Svg, { Circle } from 'react-native-svg';
 
 class NumPicker extends React.Component {
     constructor(props) {
@@ -93,12 +94,139 @@ function ButtonGroup() {
     );
 }
 
+function NowSetTime(props) {
+    return (
+        <span>{props.now}</span>
+    );
+}
+
+function TotalSetTime(props) {
+    return (
+        <span>{props.total}</span>
+    );
+}
+
+function SetDisplayBar() {
+    return (
+        <h1>
+            <NowSetTime now={2}/>
+            /
+            <TotalSetTime total={3}/>
+            セット
+        </h1>
+    );
+}
+
+function BackgroundCircle() {
+    return (
+        <Svg height="300" width="300">
+            <Circle cx="150" cy="150" r="140" strokeWidth={10}  stroke="rgb(230, 230, 230)" fill="none" strokeLinecap={"round"} />
+        </Svg>
+    );
+}
+
+function CountCircle(props) {
+    return (
+        <Svg height="300" width="300" style={styles.circleSvg}>
+            <Circle cx="150" cy="150" r="140" strokeWidth={10}  stroke="#f87c54" fill="none" strokeLinecap={"round"} strokeDasharray={props.dasharray} />
+        </Svg>
+    );
+}
+
+function NowTime(props) {
+    return (
+        <span>{props.now}</span>
+    );
+}
+
+function TotalTime(props) {
+    return (
+        <span>{props.total}</span>
+    );
+}
+
+function TimeDisplayGroup() {
+    return (
+        <p>
+            <NowTime now={2}/>
+            /
+            <TotalTime total={10}/>
+            回
+        </p>
+    );
+}
+
+function NowSecond(props) {
+    return (
+        <p>{props.nowsec}</p>
+    );
+}
+
+function PitchText(props) {
+    return (
+        <p>1回に{props.pitchsec}秒かける</p>
+    );
+}
+
+function CountNumGroup() {
+    return (
+        <div>
+            <TimeDisplayGroup/>
+            <NowSecond nowsec={0}/>
+            <PitchText pitchsec={5}/>
+        </div>
+    );
+}
+
+function IntervalMinutes(props) {
+    return (
+        <span>{props.nowminute}分</span>
+    );
+}
+
+function IntervalSeconds(props) {
+    return (
+        <span>{props.nowsecond}秒</span>
+    );
+}
+
+function IntervalNumGroup(props) {
+    return (
+        <section>
+            <h3>インターバル終了まで</h3>
+            <p>
+                <IntervalMinutes nowminute={1} />
+                <IntervalSeconds nowsecond={59} />
+            </p>
+        </section>
+    );
+}
+
+function CircleDisplayGroup() {
+    return (
+        <div>
+            <BackgroundCircle/>
+            <CountCircle dasharray={"300 876"}/>
+            <CountNumGroup/>
+            <IntervalNumGroup/>
+        </div>
+    );
+}
+
 function SettingWindow() {
     return (
         <section>
             <h1>設定画面</h1>
             <PickerCardGroup/>
-            <ButtonGroup/>
+        </section>
+    );
+}
+
+function CounterWindow() {
+    return (
+        <section>
+            <SetDisplayBar/>
+            <CircleDisplayGroup/>
         </section>
     );
 }
@@ -107,6 +235,8 @@ export default function App() {
   return (
     <SafeAreaView>
         <SettingWindow/>
+        <CounterWindow/>
+        <ButtonGroup/>
     </SafeAreaView>
   );
 }
@@ -128,4 +258,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
+    circleSvg: {
+        transform: [{ rotate: "-90deg" }]
+    }
 });
