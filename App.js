ff56c3f3905158de1daf443c6692d272d2bdea0d
中山佳重 <nakayama_yoshie@manabito.biz>
2020-06-30 00:04:05 +0900

    Support dark mode. #24

--- App.js
+++ App.js
@@ -7,13 +7,10 @@ import {Button} from "react-native-elements";
 import Svg, {Circle} from "react-native-svg";
 import {Audio} from "expo-av";
 import * as SplashScreen from "expo-splash-screen";
+import { Appearance, AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import * as Analytics from "expo-firebase-analytics";
 
 const
-    BACKGROUND_COLOR_SETTING = "#f1f0f2",
-    BACKGROUND_COLOR_COUNT = "#ffffff",
-    COLOR_MAIN = "#f87c54",
-    COLOR_INTERVAL = "#4ac08d",
     CIRCLE_STROKE_SIZE_MAX = 813,
     AUTO_SWITCH_COUNT_MAX = 600,
     COUNT_NUM_PREPARE = 5,
@@ -24,7 +21,36 @@ let
     cancelFlg = false,
     soundErrorNumFlg = false,
     soundErrorTextFlg = false,
-    autoCancelCount = 0;
+    autoCancelCount = 0,
+    textColor,
+    backgroundColorSetting,
+    backgroundColorCount,
+    backgroundColorPickerCard,
+    ColorMain,
+    ColorInterval,
+    ColorBackgroundCircle;
+
+let colorScheme = Appearance.getColorScheme();
+changeModeColor(colorScheme);
+function changeModeColor() {
+  if (colorScheme === "dark") {
+      textColor = "#eeeeee";
+      backgroundColorSetting = "#1a2744";
+      backgroundColorCount = "#1a2744";
+      backgroundColorPickerCard = "#424e69";
+      ColorMain = "#fa8f6d";
+      ColorInterval = "#72C19f";
+      ColorBackgroundCircle = "#424e69";
+  } else {
+      textColor = "#000000";
+      backgroundColorSetting = "#f1f0f2";
+      backgroundColorCount = "#ffffff";
+      backgroundColorPickerCard = "#ffffff";
+      ColorMain = "#f87c54";
+      ColorInterval = "#4ac08d";
+      ColorBackgroundCircle = "#e6e6e6";
+  }
+}
 
 class NumPicker extends React.Component {
     constructor(props) {
@@ -45,6 +71,7 @@ class NumPicker extends React.Component {
         return (
             <Picker
                 selectedValue={this.state.selected}
+                itemStyle={{color: textColor}}
                 style={styles.pickerItem}
                 onValueChange={(itemValue) => {
                     this.setState({selected: itemValue});
@@ -189,7 +216,7 @@ function BackgroundCircle() {
     return (
         <View style={styles.circlePosition}>
             <Svg height="280" width="280">
-                <Circle cx="140" cy="140" r="130" strokeWidth={14} stroke="#e6e6e6" fill="none"
+                <Circle cx="140" cy="140" r="130" strokeWidth={14} stroke={ColorBackgroundCircle} fill="none"
                         strokeLinecap={"round"}/>
             </Svg>
         </View>
@@ -284,7 +311,7 @@ class WorkoutVoiceCounter extends React.Component {
             settingPitch: 1,
             settingIntervalMinutes: 0,
             settingIntervalSeconds: 0,
-            backgroundColor: BACKGROUND_COLOR_SETTING,
+            backgroundColor: backgroundColorSetting,
             nowPrepareCount: 5,
             primaryButtonLabel: "スタート",
             secondaryButtonLabel: "キャンセル",
@@ -296,7 +323,7 @@ class WorkoutVoiceCounter extends React.Component {
             nowSetCount: 1,
             nowIntervalMinutes: 0,
             nowIntervalSeconds: 0,
-            mainKeyColor: COLOR_MAIN,
+            mainKeyColor: ColorMain,
             isCountEnd: false,
             isReady: false
         };
@@ -337,7 +364,7 @@ class WorkoutVoiceCounter extends React.Component {
           }
         ).catch(() => {
             Bugsnag.notify("Error _retrieveData");
-        })
+        });
     }
 
     /**
@@ -522,7 +549,7 @@ class WorkoutVoiceCounter extends React.Component {
     _prepareCount = () => {
         this.setState({
             nowStatus: "PREPARE",
-            backgroundColor: BACKGROUND_COLOR_COUNT,
+            backgroundColor: backgroundColorCount,
             primaryButtonLabel: "一時停止",
             secondaryButtonIsDisabled: false
         });
@@ -671,7 +698,7 @@ class WorkoutVoiceCounter extends React.Component {
         autoCancelCount = 0;
         this.setState({
             nowStatus: "SETTING",
-            backgroundColor: BACKGROUND_COLOR_SETTING,
+            backgroundColor: backgroundColorSetting,
             nowPrepareCount: 5,
             primaryButtonLabel: "スタート",
             secondaryButtonLabel: "キャンセル",
@@ -681,7 +708,7 @@ class WorkoutVoiceCounter extends React.Component {
             nowTimeCount: 0,
             nowPitchSecondCount: 0,
             nowCircleStrokeDasharray: "0 " + " " + String(CIRCLE_STROKE_SIZE_MAX),
-            mainKeyColor: COLOR_MAIN,
+            mainKeyColor: ColorMain,
             isCountEnd: false,
             isIntervalEnd: false
         });
@@ -701,7 +728,7 @@ class WorkoutVoiceCounter extends React.Component {
             nowCircleStrokeDasharray: String(circleSize) + " " + String(CIRCLE_STROKE_SIZE_MAX),
             nowIntervalMinutes: timeMinute,
             nowIntervalSeconds: timeSecond,
-            mainKeyColor: COLOR_INTERVAL,
+            mainKeyColor: ColorInterval,
             isIntervalEnd: false
         });
         const timerId = setInterval(() => {
@@ -712,7 +739,7 @@ class WorkoutVoiceCounter extends React.Component {
                 if (timeMinute === 0 && timeSecond === 1) {
                     this.setState({
                         isIntervalEnd: true,
-                        mainKeyColor: COLOR_MAIN
+                        mainKeyColor: ColorMain
                     });
                     this._playSound(false, "start");
                 }
@@ -854,7 +881,9 @@ class WorkoutVoiceCounter extends React.Component {
 
 export default function App() {
     return (
+      <AppearanceProvider>
         <WorkoutVoiceCounter/>
+      </AppearanceProvider>
     );
 }
 
@@ -896,7 +925,7 @@ const styles = StyleSheet.create({
         width: "100%"
     },
     pickerCard: {
-        backgroundColor: "#fff",
+        backgroundColor: backgroundColorPickerCard,
         borderRadius: 40,
         marginBottom: 15,
         paddingLeft: "10%",
@@ -911,11 +940,13 @@ const styles = StyleSheet.create({
         marginBottom: 150
     },
     pickerTitle: {
+        color: textColor,
         fontSize: 22,
         fontWeight: "bold",
         marginBottom: 25,
     },
     pickerText: {
+        color: textColor,
         fontSize: 18,
         paddingLeft: 2,
         paddingRight: 2
@@ -943,10 +974,12 @@ const styles = StyleSheet.create({
         width: "100%"
     },
     nowTime: {
+        color: textColor,
         fontSize: 32,
         fontWeight: "bold"
     },
     numStrong: {
+        color: textColor,
         fontSize: 58
     },
     countNumDisplay: {
@@ -959,25 +992,30 @@ const styles = StyleSheet.create({
         width: "100%"
     },
     countNum: {
+        color: textColor,
         fontSize: 26,
         fontWeight: "bold",
         marginBottom: 5
     },
     nowSecond: {
+        color: textColor,
         fontSize: 22,
         fontWeight: "bold"
     },
     nowSecondStrong: {
+        color: textColor,
         fontSize: 80
     },
     endTextStrong: {
         fontSize: 62
     },
     intervalTitle: {
+        color: textColor,
         fontSize: 18,
         fontWeight: "bold"
     },
     intervalSecondStrong: {
+        color: textColor,
         fontSize: 58
     }
 });
